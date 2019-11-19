0530380d5bb9544511e3606a1a2cf2f135697358
jjzqp <jjzqpx@163.com>
2019-11-19 15:41:25 +0800

    优化h264裸流解析代码

--- h264_parse/h264_parse.c
+++ h264_parse/h264_parse.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
+
 #define ntohl(x) (unsigned int)((((unsigned int)(x))>>24) | ((((unsigned int)(x))>>8)&0xff00) | (((unsigned int)(x))<<24) | ((((unsigned int)(x))&0xff00)<<8))
-#define ntoh(x) (unsigned hort)((((unsigned int)(x))>>8&0x00ff) | (((unsigned int)(x))<<8&0xff00))
+#define ntoh(x) (unsigned short)((((unsigned int)(x))>>8&0x00ff) | (((unsigned int)(x))<<8&0xff00))
 
 #define htonl(A)  ((((unsigned int)(A) & 0xff000000) >> 24) | \
-                  (((unsigned int)(A) & 0x00ff0000) >> 8)  | \
-                  (((unsigned int)(A) & 0x0000ff00) << 8)  | \
-                  (((unsigned int)(A) & 0x000000ff) << 24))
+                  ((( unsigned int)(A) & 0x00ff0000) >> 8)  | \
+                  ((( unsigned int)(A) & 0x0000ff00) << 8)  | \
+                  ((( unsigned int)(A) & 0x000000ff) << 24))
                   
 #define  CHANGE_264_DATA   
 
@@ -41,7 +42,7 @@ int change_buf_data(unsigned char *buf)
 }
 int main()
 {
-	#define READ_SIZE    6
+	#define READ_SIZE    64
 	printf("test...\n");
 	int ret;
 	unsigned char cache[READ_SIZE];
@@ -49,12 +50,12 @@ int main()
 	int pps_len = 0;
     int fnum = 0;
     int cur_addr = 0;
+    int flen = 0;
 
 	FILE *f = fopen("src.264","rb");
 	FILE *f_out = fopen("out.264","wb");
 	int offset = 0;
 	int cnt = 0;
-	//fseek(f,cur_addr,SEEK_CUR);
 	unsigned char *buf = (unsigned char *)malloc(1024*1024);
 	while(1) {
 		ret = fread(cache,READ_SIZE,1,f);
@@ -84,6 +85,7 @@ int main()
         	//printf("cur_addr==%x\n",cur_addr);
         	change_buf_data(buf);
         	fnum++;
+        	fwrite(buf,offset,1,f_out);
 #ifndef CHANGE_264_DATA
         } else if ((cache[0] == 0) && (cache[4] == 0x67)) { 
         	memcpy(&offset, &cache[0], 4);
@@ -140,38 +142,23 @@ int main()
         } else if ((cache[0] == 0) && (cache[4] == 0x67)) {
             //sps
             printf("sps\n");
+            printf_buf(cache,32);
             unsigned int tmp_addr = cur_addr;
             memcpy(&sps_len, &cache[0], 4);
             sps_len = ntohl(sps_len);	
-            sps_len += 4;
-            ret = fread(buf,sps_len,1,f);
-            tmp_addr += sps_len;
-            
-            ret = fread(cache,READ_SIZE,1,f);
-            tmp_addr += READ_SIZE;
-            fseek(f, tmp_addr-READ_SIZE, SEEK_SET);
-            tmp_addr -= READ_SIZE;
-            if ((cache[0] == 0) && (cache[4] == 0x68)) {
+            if ((cache[sps_len+4] == 0) && (cache[(sps_len+4)+4] == 0x68)) {
                 //pps
                 printf("pps\n");
-                memcpy(&pps_len, &cache[0], 4);
+                memcpy(&pps_len, &cache[sps_len+4], 4);
                 pps_len = ntohl(pps_len);
-                pps_len += 4;
-                ret = fread(buf,pps_len,1,f); 
-                printf_buf(buf,pps_len);
-                tmp_addr += pps_len;
-                ret = fread(cache,READ_SIZE,1,f);
-                tmp_addr += READ_SIZE;
-                fseek(f, tmp_addr-READ_SIZE, SEEK_SET);
-                tmp_addr -= READ_SIZE;
-                if ((cache[0] == 0) && (cache[4] == 0x65)) {
-                    printf("III\n");
-                    memcpy(&offset, &cache[0], 4);
-                    offset = ntohl(offset);
-                    offset = offset + sps_len + pps_len;
-                    fseek(f, cur_addr, SEEK_SET);
-                    ret = fread(buf,offset,1,f);
-                    cur_addr += offset;
+                printf_buf(&pps_len,4);
+                if ((cache[(sps_len+4)+pps_len+4] == 0) && (cache[(sps_len+4)+pps_len+4+4] == 0x65)) {
+                    printf("III\n");   
+                    memcpy(&offset, &cache[(sps_len+4)+pps_len+4], 4);                   
+                    offset = ntohl(offset);                 
+                    flen = offset + sps_len + pps_len + 8;
+                    ret = fread(buf,flen,1,f);
+                    cur_addr += flen;
                     printf("cur_addr==%x\n",cur_addr);
                        if(ret == 0){
                         printf("end\n");
@@ -183,7 +170,7 @@ int main()
                 
             }
             
-            fwrite(buf,offset,1,f_out);
+            fwrite(buf,flen,1,f_out);
         }
 
 #endif
@@ -195,6 +182,9 @@ __END:
 	}
 	if(f_out){
 		fclose(f_out);
+	}
+	if(buf){
+		free(buf);
 	}
 	
 	return 0;
